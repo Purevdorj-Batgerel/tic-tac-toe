d327418dee91955efa44e6bbb8e08a26f264da68
Purevdorj Batgerel <purvee.hq@gmail.com>
2019-03-18 18:15:41 -0500

    [Imp] Made game winnable

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -27,7 +27,7 @@ class Game extends React.Component {
     let oldArr = [...this.state.current.cells];
     let newArr = [...this.state.current.cells];
 
-    if (oldArr[i] !== null) {
+    if (this.calcWinner(newArr) || oldArr[i] !== null) {
       return;
     }
 
@@ -40,9 +40,43 @@ class Game extends React.Component {
     });
   }
 
+  calcWinner(cells) {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (const condition of winConditions) {
+      const [a, b, c] = condition;
+      if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   render() {
     const { current } = this.state;
-    const status = `${this.state.turn}'s turn`;
+    let status;
+    let finish = this.calcWinner(current.cells);
+
+    if (finish) {
+      status = `${this.switchTurn()} won`;
+    } else {
+      if (this.state.history.length == 9) {
+        status = 'TIE';
+        finish = true;
+      } else {
+        status = `${this.state.turn}'s turn`;
+      }
+    }
+
     return (
       <div id="game">
         <Title title="Tic - Tac - Toe" />
