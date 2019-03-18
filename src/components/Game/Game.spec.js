a50a303acd147131e07d2a32d8950469057e81d7
Purevdorj Batgerel <purvee.hq@gmail.com>
2019-03-18 17:39:12 -0500

    [Add] spec click on same cell twice

--- src/components/Game/Game.spec.js
+++ src/components/Game/Game.spec.js
@@ -31,4 +31,36 @@ describe('<MyComponent />', () => {
     expect(state.turn).to.equal('O');
     expect(state.history.length).to.equal(1);
   });
+
+  it('simulates click events on same cell', () => {
+    const wrapper = mount(<Game />);
+
+    wrapper
+      .find('.cell')
+      .at(0)
+      .simulate('click');
+
+    wrapper
+      .find('.cell')
+      .at(0)
+      .simulate('click');
+
+    wrapper.update();
+
+    const state = wrapper.state();
+
+    expect(state.current.cells).to.eql([
+      'X',
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ]);
+    expect(state.turn).to.equal('O');
+    expect(state.history.length).to.equal(1);
+  });
 });
